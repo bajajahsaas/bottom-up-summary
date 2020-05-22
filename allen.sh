0aacb3335a9d18499657a27211316424f848b1e7
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-05-22 18:23:01 -0400

    update gpu

--- allen.sh
+++ allen.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=logsallen/copy_%j.txt  # output file
 #SBATCH -e logsallen/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:3
-#SBATCH --partition=m40-short # Partition to submit to
+#SBATCH --partition=2080ti-long # Partition to submit to
 #SBATCH --mem=40000
 #
 #SBATCH --ntasks=1
