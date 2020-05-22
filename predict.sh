3a179ea86c7262dd8f5b612df8093be94329a57d
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-05-22 18:13:50 -0400

    refactor

--- predict.sh
+++ predict.sh
@@ -1,5 +1,5 @@
 modelfile=simple_tagger/model.tar.gz
 datafile=data/processed/multicopy.test.pred.txt
-outputfile=simple_tagger_output.txt
+outputfile=outputs/simple_tagger_output.txt
 
 python -m allennlp.run predict $modelfile $datafile --output $outputfile --cuda-device -1 --batch-size 50
